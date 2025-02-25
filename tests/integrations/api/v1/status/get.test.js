24ebc359b59b356bb9ddb415f90eeb1b95b31b36
Guilherme Pereira <guilherme@hidroluzvpn.com.br>
2025-02-25 14:50:32 -0300

    test: rename all descriptions to use new pattern

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -5,17 +5,21 @@ beforeAll(async () => {
   return;
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(responseBody.update_at).toBeDefined();
-  const parseUpdateAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toBe(parseUpdateAt);
+      expect(responseBody.update_at).toBeDefined();
+      const parseUpdateAt = new Date(responseBody.update_at).toISOString();
+      expect(responseBody.update_at).toBe(parseUpdateAt);
 
-  expect(responseBody.dependecies.database.version).toEqual("16.0");
-  expect(responseBody.dependecies.database.max_connections).toEqual(100);
-  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependecies.database.version).toEqual("16.0");
+      expect(responseBody.dependecies.database.max_connections).toEqual(100);
+      expect(responseBody.dependecies.database.opened_connections).toEqual(1);
+    });
+  });
 });
